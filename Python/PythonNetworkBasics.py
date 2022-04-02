58fa179c42855110d9e8452dc6b48939ffc12877
Hommy Rosado <hommy.rosado@gmail.com>
2022-04-02 17:46:53 -0400

    Update.

---
+++ Python/PythonNetworkBasics.py
@@ -0,0 +1,10 @@
+protocolList = ["ftp", "ssh", "smtp", "http"]
+
+# protocolList.sort()
+print(protocolList)
+print(type(protocolList))
+print(len(protocolList))
+
+position = protocolList.index("ssh")
+
+print(position)
